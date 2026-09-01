ef81573a7ad4fc742078155fc8d13fc0666a2c12
Jeff Morgan <jmorgan415@gmail.com>
2026-08-31 18:04:55 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <img id="portrait" alt="Portrait" src="./assets/portrait.jpg" />
+  <SafeFlightHGSchool msg="Welcome to the SafeFlight Hang Gliding School!" />
+</template>
+
+<script>
+import SafeFlightHGSchool from "./components/SafeFlightHGSchool.vue";
+
+export default {
+  name: "App",
+  components: {
+    SafeFlightHGSchool,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+#portrait {
+  border-radius: 25px;
+  filter: drop-shadow(0 0 0.2rem gray);
+}
+</style>
---
+++ src/components/SafeFlightHGSchool.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Days Since Last Accident: 0</h2>
+    <h3>Essential Links</h3>
+    <ul>
+      <li>
+        <a href="#" rel="noopener">See How</a>
+      </li>
+      <li>
+        <a href="#" rel="noopener">Support Us</a>
+      </li>
+      <li>
+        <a href="#" rel="noopener">Forum</a>
+      </li>
+      <li>
+        <a href="#" rel="noopener">News</a>
+      </li>
+      <li>
+        <a href="https://twitter.com/wiz_io" target="_blank" rel="noopener">Twitter</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SafeFlightHGSchool",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
